bd34d0c81da6d8d98c79483030e7d7d6eecb1e26
zulkarom <zulkarom@gmail.com>
2019-10-06 01:47:32 +0800

    admin module change

---
+++ backend/modules/admin/controllers/AssignmentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace  backend\modules\admin\controllers;
+
+
+use mdm\admin\controllers\AssignmentController as BaseAssignmentController;
+
+class AssignmentController extends BaseAssignmentController
+{
+   public function actionIndex()
+    {
+		
+		return parent::actionIndex();
+	}
+}
