d2163a60f6175e2b410854d0122c6e9140f89898
zulkarom <zulkarom@gmail.com>
2019-11-06 15:22:23 +0800

    Create params.php

---
+++ table - wea/common/config/params.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'adminEmail' => 'admin@example.com',
+    'supportEmail' => 'support@example.com',
+    'user.passwordResetTokenExpire' => 3600,
+	'faculty_id' => 1,
+	'faculty_abbr' => 'FKP'	
+];
