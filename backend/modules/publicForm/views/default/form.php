bf4413034b9d5b7f0b6a11247c238810292fca41
zulkarom <zulkarom@gmail.com>
2020-03-24 10:17:39 +0800

    time remaining off

--- backend/modules/publicForm/views/default/form.php
+++ backend/modules/publicForm/views/default/form.php
@@ -26,12 +26,17 @@ Due Date: <?=date('d M Y' , strtotime($setting->date_end))?>
 <br />
 Time Remaining: <span style="color:red">
 <?php 
-$now = new \DateTime();
+if(strtotime($setting->date_end) < time()){
+	echo '---';
+}else{
+	$now = new \DateTime();
 $future_date = new \DateTime($setting->date_end . ' 11:59:59');
 //$future_date->modify('+ 8 hours');
 $interval = $future_date->diff($now);
 
 echo $interval->format("%a days, %h hours");
+}
+
 
 ?>
 </span>
