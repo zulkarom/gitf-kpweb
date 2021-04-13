c5cf94e07c637fd4213886930d692cba7469397f
zulkarom <zulkarom@gmail.com>
2021-04-14 01:12:30 +0800

    xxx

--- backend/modules/teachingLoad/models/AppointmentLetterFile.php
+++ backend/modules/teachingLoad/models/AppointmentLetterFile.php
@@ -245,7 +245,7 @@ EOD;
 	public function writeSignitureImg(){
 		$html = '';
 		$sign = $this->template->signiture_file;
-		$html .= $sign;
+		$html .= 'xxxxxxxxxxxx' . $sign;
 		if(!$sign){
 			//return false;
 		}
