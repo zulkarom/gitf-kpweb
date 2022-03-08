b10b97f297dc3ae920f925b4e81c77929641e104
zulkarom <zulkarom@gmail.com>
2022-03-08 16:17:29 +0800

    manual module

---
+++ backend/modules/manual/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace backend\modules\manual;
+
+/**
+ * manual module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'backend\modules\manual\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
