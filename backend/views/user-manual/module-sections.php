6df7d566f7a6818b4b8886ca4494bff25e18e50b
zulkarom <zulkarom@gmail.com>
2022-03-14 11:51:28 +0800

    new

--- backend/views/user-manual/module-sections.php
+++ backend/views/user-manual/module-sections.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
 <h3><?=$module->module_name?></h3>
 
 <?php 
-
+$web = Yii::getAlias('@web');
 echo '<ul>';
 foreach($sections as $s){
     echo '<li>' . Html::a($s->section_name, ['titles', 's' => $s->id]) . '</li>';
@@ -18,7 +18,12 @@ foreach($sections as $s){
     if($titles){
         echo '<ul>';
         foreach($titles as $t){
-            echo '<li style="font-size:16px"><i>' . Html::a($t->title_text, ['item-steps', 't' => $t->id]) . '</i></li>';
+            if($t->is_new){
+                $new = ' <img src="'.$web.'/images/new.png" style="display:inline" />';
+            }else{
+                $new = '';
+            }
+            echo '<li style="font-size:16px"><i>' . Html::a($t->title_text . ' ' . $new, ['item-steps', 't' => $t->id]) . '</i></li>';
         }
         echo '</ul>';
     }
