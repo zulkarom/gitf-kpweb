7d4891735cde4b9a5aba9e1e854083e03ab59bda
zulkarom <zulkarom@gmail.com>
2022-03-16 17:03:49 +0800

    x

--- backend/modules/postgrad/views/student/view.php
+++ backend/modules/postgrad/views/student/view.php
@@ -177,8 +177,32 @@ Semester
 </div>
 <div class="box-body">
 
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Semester</th>
+      <th scope="col">Status</th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
 
+  </tbody>
+</table>
+
+
+
+<div class="form-group">
 <a href="<?=Url::to(['student-semester/create', 's' => $model->id])?>" class="btn btn-primary btn-sm">New Semester</a>
+</div>
+
 
 
 </div>
