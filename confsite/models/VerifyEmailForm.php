0028aa7d61d5a6fecd02a13eeff580f1e89a9341
zulkarom <zulkarom@gmail.com>
2022-06-27 15:13:48 +0800

    ss

--- confsite/models/VerifyEmailForm.php
+++ confsite/models/VerifyEmailForm.php
@@ -29,11 +29,11 @@ class VerifyEmailForm extends Model
     public function __construct($token, array $config = [])
     {
         if (empty($token) || !is_string($token)) {
-            throw new InvalidArgumentException('Verify email token cannot be blank.');
+            //throw new InvalidArgumentException('Verify email token cannot be blank.');
         }
         $this->_user = User::findByVerificationToken($token);
         if (!$this->_user) {
-            throw new InvalidArgumentException('Wrong verify email token.');
+            //throw new InvalidArgumentException('Wrong verify email token.');
         }
         parent::__construct($config);
     }
