234fd15bb6d69f0b4c8b7057c99d2b685dc7abe2
zulkarom <zulkarom@gmail.com>
2023-05-31 00:14:26 +0800

    uc

--- tu2i/views/layouts/main-login.php
+++ tu2i/views/layouts/main-login.php
@@ -1,13 +1,13 @@
 <?php
 use common\widgets\Alert;
 use yii\helpers\Html;
-use tu2i\assets\tu2iAsset;
+use tu2i\assets\Tu2iAsset;
 use tu2i\assets\AppAsset;
 use kartik\widgets\ActiveForm;
 
-tu2iAsset::register($this);
+Tu2iAsset::register($this);
 AppAsset::register($this);
-$dirAssests = Yii::$app->assetManager->getPublishedUrl('@tu2i/assets/tu2iAssets');
+$dirAssests = Yii::$app->assetManager->getPublishedUrl('@tu2i/assets/Tu2iAssets');
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
