942b8ebd40724cc4af303f13d872b9e1050229e5
zulkarom <zulkarom@gmail.com>
2024-09-05 09:51:34 +0800

    d

--- common/models/User.php
+++ common/models/User.php
@@ -190,7 +190,7 @@ class User extends ActiveRecord implements IdentityInterface
 
         return static::findOne([
             'password_reset_token' => $token,
-            'status' => self::STATUS_ACTIVE,
+            //'status' => self::STATUS_ACTIVE,
         ]);
     }
 
