38ac8cf7ecb1f5a2dc072d7a35f70648cff16d13
Zul Karami <zulkarom@gmail.com>
2025-12-17 13:03:00 +0800

    d

---
+++ backend/controllers/SettingController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\helpers\FileHelper;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\web\UploadedFile;
+
+class SettingController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function actionUploadTest()
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+
+        if (!Yii::$app->request->isPost) {
+            return [
+                'ok' => false,
+                'error' => 'POST a multipart/form-data request with field name "file"',
+            ];
+        }
+
+        $file = UploadedFile::getInstanceByName('file');
+        if (!$file) {
+            return [
+                'ok' => false,
+                'error' => 'No file uploaded (expected field name: file)',
+            ];
+        }
+
+        $directory = Yii::getAlias('@runtime/setting-upload-test/');
+        if (!is_dir($directory)) {
+            FileHelper::createDirectory($directory);
+        }
+
+        $token = bin2hex(random_bytes(8));
+        $safeExt = preg_replace('/[^a-zA-Z0-9]/', '', (string)$file->extension);
+        $fileName = 'upload-test-' . $token . ($safeExt ? '.' . strtolower($safeExt) : '');
+        $path = $directory . $fileName;
+
+        if (!$file->saveAs($path)) {
+            return [
+                'ok' => false,
+                'error' => 'Failed to save file to runtime directory',
+            ];
+        }
+
+        return [
+            'ok' => true,
+            'name' => $file->name,
+            'size' => $file->size,
+            'extension' => $file->extension,
+            'saved_as' => $path,
+        ];
+    }
+}
